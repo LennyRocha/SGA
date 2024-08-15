2980340f7d11db9d081dd533d4a5e5e44e53ff66
CiscoPaquitoTracer <20233tn239@utez.edu.mx>
2024-08-15 15:41:05 -0600

    Push y commit de prueba

--- Integradora/src/main/java/mx/edu/utez/integradora/Utils/DatabaseConnectionManager.java
+++ Integradora/src/main/java/mx/edu/utez/integradora/Utils/DatabaseConnectionManager.java
@@ -8,8 +8,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnectionManager {
     private static final String JDBC_URL = "jdbc:mysql://localhost/SGADB";
-    private static final String USERNAME = "HP-KENNY";
-    private static final String PASSWORD = "2005";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
     private static final HikariConfig config = new HikariConfig();
     private static final HikariDataSource dataSource;
 
